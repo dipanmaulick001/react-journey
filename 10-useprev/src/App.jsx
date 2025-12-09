b50ec2424f82ea1659629f1e5e144a3c8f5d321b
Dipan <dipanmaulick25@gmail.com>
2025-12-09 07:13:17 +0000

    added 10-useprev : how usePrev works

---
+++ 10-useprev/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+import { usePrev } from './hooks/useprev'
+
+function App(){
+  const [count , setCount] = useState(0);
+  const prev = usePrev(count);
+
+  return <>
+    <p>{count}</p>
+    <button onClick={()=>setCount(count=>count+1)}>Click me</button>
+    <p>Previous value : {prev}</p>
+  </>
+
+}
+
+export default App
